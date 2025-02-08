37179480aafb34c3c3e43c870ce2560e2367c7d5
mt0211 <vothong123pq@gmail.com>
2025-02-08 21:26:57 +0700

    dashboard 90%

---
+++ ClinicDasboardAPI/DTOs/DataDTOs/DataDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicDasboardAPI.DTOs.DataDTOs
+{
+    public class DataDTO
+    {
+        public int totalUser { get; set; }
+        public int totalPet { get; set; }
+        public decimal monthlyRevenue { get; set; }
+        public float avgRating { get; set; }
+    }
+}
---
+++ ClinicDasboardAPI/Repositories/IClinicDashboardRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Models;
+
+namespace ClinicDasboardAPI.Repositories
+{
+    public interface IClinicDashboardRepository
+    {
+        Task<(int userCount, int petCount, decimal totalRevenue, float avgRating)> GetDataCount();
+        Task<User> GetUserByID(Guid id);
+        Task<(int pending, int confirmed, int completed, int canceled)> GetAppointmentStatistic(DateTime? date);
+    }
+}
