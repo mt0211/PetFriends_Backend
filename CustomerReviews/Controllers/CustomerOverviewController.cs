af6c682a67d019fa22ba7e3a64c43a2b9b397a02
mt0211 <vothong123pq@gmail.com>
2025-02-10 21:38:18 +0700

    done clinic rating overview temporary

---
+++ CustomerReviews/Controllers/CustomerOverviewController.cs
@@ -0,0 +1,39 @@
+﻿using CustomerReviews.DTOs.ResultModel;
+using CustomerReviews.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerReviews.Controllers
+{
+    [ApiController]
+    [Route("api/customeroverview")]
+    public class CustomerOverviewController : ControllerBase
+    {
+        private readonly ICustomerOverviewService _service;
+        public CustomerOverviewController(ICustomerOverviewService service)
+        {
+            _service = service;
+        }
+        [HttpGet("review-list")]
+        public async Task<IActionResult> GetFeedbackList(int page)
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            if (string.IsNullOrEmpty(token))
+            {
+                return BadRequest("Unable to retrieve user ID");
+            }
+            ResultModel result = await _service.GetListReviews(token, page);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+        [HttpGet("rating-overview")]
+        public async Task<IActionResult> GetRatingOverview()
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            if (string.IsNullOrEmpty(token))
+            {
+                return BadRequest("Unable to retrieve user ID");
+            }
+            ResultModel result = await _service.GetRatingOverView(token);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+    }
+}
