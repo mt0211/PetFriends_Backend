5a863292d397348f900732cb6d64a132aa4d7a40
mt0211 <vothong123pq@gmail.com>
2025-03-20 15:41:15 +0700

    fix api pet and export to excel admin report

--- AdminReport/Services/IAdminReportService.cs
+++ AdminReport/Services/IAdminReportService.cs
@@ -8,5 +8,6 @@ namespace AdminReport.Services
         Task<ResultModel> GetDataNewUser(string token, int year, int month);
         Task<ResultModel> GetPostDistribution(string token);
         Task<ResultModel> GetSystemVaccineCount(string token);
+        Task<ResultModel> ExportAllReportsToExcel(string token, int year, int? month);
     }
 }
