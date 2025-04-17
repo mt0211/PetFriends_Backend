a6fe7800b00012d0eda039311f43594a96f76bed
mt0211 <vothong123pq@gmail.com>
2025-04-17 08:16:43 +0700

    added expo push token

---
+++ DataAccess/Models/UserPushToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+public partial class UserPushToken
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string Token { get; set; } = null!;
+
+    public string? DeviceType { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
